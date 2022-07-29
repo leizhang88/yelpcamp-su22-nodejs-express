7e4933d146629a53c20300803e0d86866bc1936c
leiz <callmelei@outlook.com>
2022-07-29 14:26:15 -0700

    Add joi validation for campground

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require("path");
 const mongoose = require("mongoose");
 const methodOverride = require("method-override");
 const ejsMate = require("ejs-mate");
-const Joi = require("joi");
 const Campground = require("./models/campground");
 const catchAsync = require("./utils/catchAsync");
 const ExpressError = require("./utils/ExpressError");
 const { object } = require("joi");
+const { campgroundSchema } = require("./utils/joi-schemas");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp")
 
@@ -26,6 +26,18 @@ app.set("views", path.join(__dirname, "views"))
 app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride("_method"))
 
+
+const validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(e => e.message).join(', ');
+        throw new ExpressError(msg, 400);
+    } else {
+        next()
+    }
+}
+
+
 app.get("/", (req, res) => {
     res.render("home");
 })
@@ -39,8 +51,7 @@ app.get("/campgrounds/new", (req, res) => {
     res.render("campgrounds/new");
 })
 
-app.post("/campgrounds", catchAsync(async (req, res, next) => {
-    if (!req.body.campground) { throw new ExpressError("Invalid campground data", 400) }
+app.post("/campgrounds", validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
@@ -56,7 +67,7 @@ app.get("/campgrounds/:id/edit", catchAsync(async (req, res) => {
     res.render("campgrounds/edit", { campground });
 }))
 
-app.put("/campgrounds/:id", catchAsync(async (req, res) => {
+app.put("/campgrounds/:id", validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndUpdate(id, { ...req.body.campground });
     res.redirect(`/campgrounds/${id}`)

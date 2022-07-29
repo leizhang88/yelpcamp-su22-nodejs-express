716282094b74cf862a5c8b664289c28ece974980
leiz <callmelei@outlook.com>
2022-07-28 22:34:25 -0700

    Add boilerplate and partials and bootstrap styling

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,13 @@ const seedDb = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i++) {
         const rand = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 35) + 10;
         const cg = new Campground({
             location: `${cities[rand].city}, ${cities[rand].state}`,
-            title: `${sample(descriptors)} ${sample(places)}`
+            title: `${sample(descriptors)} ${sample(places)}`,
+            price,
+            image: 'https://source.unsplash.com/1600x900?camping',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Error, doloribus quis repellat quam consequatur illum aut. Minima voluptas eveniet consectetur exercitationem facilis, distinctio optio dolore voluptates debitis rem autem molestias.'
         })
         await cg.save();
     }

9dade577ffa59edda9812e35aeba6c441134b035
leiz <callmelei@outlook.com>
2022-08-02 11:10:57 -0700

    Add isReviewAuthor middleware

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,11 +4,12 @@ const Campground = require("../models/campground");
 const Review = require("../models/review");
 const catchAsync = require("../utils/catchAsync");
 const ExpressError = require("../utils/ExpressError");
-const { validateReview } = require("../utils/middleware");
+const { validateReview, isLoggedIn, isReviewAuthor } = require("../utils/middleware");
 
-router.post("/", validateReview, catchAsync(async (req, res) => {
+router.post("/", isLoggedIn, validateReview, catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.cid);
     const review = new Review(req.body.review);
+    review.author = req.user._id;
     campground.reviews.push(review);
     await review.save();
     await campground.save();
@@ -16,7 +17,7 @@ router.post("/", validateReview, catchAsync(async (req, res) => {
     res.redirect(`/campgrounds/${campground._id}`)
 }))
 
-router.delete("/:rid", catchAsync(async (req, res) => {
+router.delete("/:rid", isLoggedIn, isReviewAuthor, catchAsync(async (req, res) => {
     const { cid, rid } = req.params;
     await Campground.findByIdAndUpdate(cid, { $pull: { reviews: rid } });
     await Review.findByIdAndDelete(rid);
--- utils/middleware.js
+++ utils/middleware.js
@@ -1,6 +1,7 @@
 const { campgroundSchema, reviewSchema } = require("./joi-schemas");
 const ExpressError = require("./ExpressError");
 const Campground = require("../models/campground");
+const Review = require('../models/review');
 
 module.exports.validateReview = (req, res, next) => {
     const { error } = reviewSchema.validate(req.body);
@@ -37,6 +38,16 @@ module.exports.isAuthor = async (req, res, next) => {
     if (!campground.author.equals(req.user._id)) {
         req.flash('error', 'Permission required!');
         return res.redirect(`/campgrounds/${id}`)
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { cid, rid } = req.params;
+    const review = await Review.findById(rid);
+    if (!review.author.equals(req.user._id)) {
+        req.flash('error', 'Permission required!');
+        return res.redirect(`/campgrounds/${cid}`)
     }
     next();
 }
